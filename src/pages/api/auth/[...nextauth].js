7c63908696902d55c97fbe8c8e8f4ab58efd2e51
selvaganapathyvikki <80957326+selvaganapathyvikki@users.noreply.github.com>
2021-12-20 21:57:27 +0530

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,8 +7,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: 89214794816-61ci8vjmjpv8iggtc0l0liv979i3r01d.apps.googleusercontent.com,
-      clientSecret: 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, 
+      clientId: "89214794816-61ci8vjmjpv8iggtc0l0liv979i3r01d.apps.googleusercontent.com",
+      clientSecret: "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", 
     }),
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
